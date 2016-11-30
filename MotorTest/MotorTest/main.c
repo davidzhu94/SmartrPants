48f61936c731ca7e02dd4272583d72e2cd11957d
jwoos001 <jwoos001@ucr.edu>
2016-11-29 20:56:02 -0800

    added usart

--- MotorTest/MotorTest/main.c
+++ MotorTest/MotorTest/main.c
@@ -1,14 +1,15 @@
-//FreeRTOS include files
 #include "FreeRTOS.h"
 #include "task.h"
 #include "croutine.h"
 #include "usart_ATmega1284.h"
 #include <util/delay.h>
 enum MotorState {INIT,Wait, Pull, Reverse} motor_state;
-	
+
 unsigned short pullTimer = 0;
 unsigned short reverseTimer = 0;
 unsigned char temp;
+unsigned char password[] = {'1','3','3','7'};
+unsigned char password_count = 0;
 
 void Motor_Init(){
 	motor_state = INIT;
@@ -18,8 +19,8 @@ void Motor_Tick(){
 	//Actions
 	switch(motor_state){
 		case INIT:
-		break;
-		case Wait:	
+			break;
+		case Wait:
 			break;
 		case Pull:
 			PORTA = 0x01;
@@ -34,38 +35,46 @@ void Motor_Tick(){
 			PORTA = 0x02;
 			_delay_ms(1);
 			reverseTimer++;
-			break;
+		break;
 	}
 	//Transitions
 	switch(motor_state){
 		case INIT:
-			motor_state = Wait;
-			break;
+		motor_state = Wait;
+		break;
 		case Wait:
-			if(USART_HasReceived(0))
+		if(USART_HasReceived(0))
+		{
+			temp = USART_Receive(0);
+			if(temp == 0x01)
 			{
-				temp = USART_HasReceived(0);
-				if(temp == 0)
-					motor_state = Pull;
-				if(temp == 1)
-					motor_state = Reverse;
-				USART_Flush(0);
+				motor_state = Pull;
+				PORTB = 0x01;
 			}
-			break;
+			if(temp == 0x02)
+			{
+				motor_state = Reverse;
+				PORTB = 0x02;
+			}
+			USART_Flush(0);
+		}
+		break;
 		case Pull:
-			if(pullTimer == 5000)
-			{
-				pullTimer = 0;
-				motor_state = Wait;
-			}
-			break;
+		if(pullTimer == 1500)
+		{
+			pullTimer = 0;
+			PORTB = 0x00;
+			motor_state = Wait;
+		}
+		break;
 		case Reverse:
-			if(reverseTimer == 5000)
-			{
-				reverseTimer = 0;
-				motor_state = Wait;
-			}
-			break;
+		if(reverseTimer == 1500)
+		{
+			reverseTimer = 0;
+			PORTB = 0x00;
+			motor_state = Wait;
+		}
+		break;
 	}
 }
 
@@ -87,6 +96,7 @@ void StartSecPulse(unsigned portBASE_TYPE Priority)
 int main(void)
 {
 	DDRA = 0xFF; PORTA=0x00;
+	DDRB = 0xFF; PORTB=0x00;
 	DDRD = 0x00; PORTD = 0xFF;
 	initUSART(0);
 	//Start Tasks
